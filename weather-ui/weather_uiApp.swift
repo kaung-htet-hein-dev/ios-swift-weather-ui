541fc31da3290f1944ed7844111f5f07d562b82a
Kaung-Htet-Hein <kaunghtethein.dev@gmail.com>
2025-07-01 22:06:13 +0800

    Initial Commit

---
+++ weather-ui/weather_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_uiApp.swift
+//  weather-ui
+//
+//  Created by Kaung Htet Hein on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
